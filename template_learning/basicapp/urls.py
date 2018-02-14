230b0b0666cba85091ccfc503edb048932059fa3
Chandrabhushan Kesavabhotla <chandrabhushan@Chandrabhushans-MacBook-Pro.local>
2018-02-14 15:04:27 +0530

    first

---
+++ template_learning/basicapp/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from basicapp import views
+
+# template tagging
+app_name = 'basicapp'
+
+urlpatterns = [
+
+    url(r'^relative/$',views.relative,name='relative'),
+    url(r'^other/$',views.other,name='other'),
+
+
+
+]
